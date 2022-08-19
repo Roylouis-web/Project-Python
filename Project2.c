0927ad02f542afe5aa458ae85b8f4b2ee933bb87
Roylouis-web <powellleroy488@gmail.com>
2022-08-20 00:31:21 +0100

    My first basic Python Project

---
+++ Project2.c
@@ -0,0 +1,15 @@
+#import <stdio.h>
+
+int main(){
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+puts('I love this world\n');
+
+return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+My first Python project 
+This python project of mine is going to be life changing one because it was assigned to me by Microsoft, one of the most prestigious and respected tech companies.
